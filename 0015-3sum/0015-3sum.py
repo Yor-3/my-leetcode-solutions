1232547f16a1c593a8c0a79603416fdd0068ebf0
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-03-30 15:31:54 +0530

    Time: 536 ms (74.79%), Space: 20.8 MB (23.46%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,29 +1,34 @@
+from typing import List
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-
         nums.sort()
 
-        for i,a in enumerate(nums):
-            if i>0 and a == nums[i-1]:
-                continue
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue  # Skip duplicates for `a`
 
-            l = i+1
-            r = len(nums)-1
+            l, r = i + 1, len(nums) - 1  # ← Correctly define `l` and `r` here
 
-            while l<r:
-                s = a+nums[l]+nums[r]
+            while l < r:
+                s = a + nums[l] + nums[r]
 
-                if s>0:
-                    r= r-1
-
-                elif s<0:
-                    l = l+1
-
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
                 else:
-                    res.append([a,nums[l],nums[r]])
-                    l+=1
-                    while l<r and nums[l] == nums[l-1]:
-                        l=l+1
+                    res.append([a, nums[l], nums[r]])  # ← Use list brackets `[]`
+
+                    # Skip duplicates for `l` and `r`
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+
+                    # Move both pointers after finding a valid triplet
+                    l += 1
+                    r -= 1
 
         return res
