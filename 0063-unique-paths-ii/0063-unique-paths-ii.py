ff8e4ef79d6a637cb9ed79a5c52030b76aff393e
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-04-16 19:33:34 +0530

    Time: 0 ms (100%), Space: 18 MB (29.33%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,17 +3,19 @@ from typing import List
 class Solution:
     def uniquePathsWithObstacles(self, g: List[List[int]]) -> int:
         m, n = len(g), len(g[0])
-        dp = [[0 for _ in range(n)] for _ in range(m)]
+        prev = [0] * n
 
         for i in range(m):
+            cur = [0] * n
             for j in range(n):
                 if g[i][j] == 1:
-                    dp[i][j] = 0  
+                    cur[j] = 0  
                 elif i == 0 and j == 0:
-                    dp[i][j] = 1  
+                    cur[j] = 1  
                 else:
-                    up = dp[i - 1][j] if i > 0 else 0
-                    left = dp[i][j - 1] if j > 0 else 0
-                    dp[i][j] = up + left
+                    up = prev[j] if i > 0 else 0
+                    left = cur[j - 1] if j > 0 else 0
+                    cur[j] = up + left
+            prev = cur
 
-        return dp[m - 1][n - 1]
+        return prev[n - 1]
