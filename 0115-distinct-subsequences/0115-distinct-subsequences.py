8ad5980008cec1801c88f680d2a9e983518778ce
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-04-19 23:29:49 +0530

    Time: 279 ms (83.81%), Space: 18 MB (92.28%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,33 +1,16 @@
 class Solution:
-    # def f(self,i,j,s,t,dp):
-    #     if j<0:
-    #         return 1
-    #     if i<0:
-    #         return 0
-
-    #     if dp[i][j]!=-1:
-    #         return dp[i][j]
-    #     if s[i]==t[j]:
-    #         dp[i][j] =  self.f(i-1,j-1,s,t,dp)+self.f(i-1,j,s,t,dp)
-    #         return dp[i][j]
-    #     dp[i][j] = self.f(i-1,j,s,t,dp)
-    #     return dp[i][j]
     def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        prev = [0] * (n + 1)
+        prev[0] = 1
 
-        m= len(s)
-        n=len(t)
-
-        
-
-        dp = [[0 for _ in range(n+1)]for _ in range(m+1)]
-        for i in range(m + 1):
-            dp[i][0] = 1
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                if s[i-1]==t[j-1]:
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j]
+        for i in range(1, m + 1):
+            cur = prev[:]
+            for j in range(n, 0, -1):
+                if s[i - 1] == t[j - 1]:
+                    cur[j] = prev[j - 1] + prev[j]
                 else:
-                    dp[i][j] = dp[i-1][j]
+                    cur[j] = prev[j]
+            prev = cur
 
-        return dp[m][n]
-        
+        return prev[n]
