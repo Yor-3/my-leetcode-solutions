2a84b3ca2a2719ccb002a1c2aa52c97c888c20b8
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-04-19 21:09:38 +0530

    Time: 1539 ms (6.56%), Space: 19.3 MB (30.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,25 +1,25 @@
 class Solution:
-    def f(self, i, a, c,dp):
-        if i == 0:
-            if a % c[0] == 0:
-                return a // c[0]
-            else:
-                return float('inf')
-
-        if dp[i][a]!=-1:
-            return dp[i][a]
-        notpick = self.f(i - 1, a, c,dp)
-        pick = float('inf')
-        if a >= c[i]:
-            pick = 1 + self.f(i, a - c[i], c,dp)
-
-
-        dp[i][a]= min(pick, notpick)
-        return dp[i][a]
-
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n=len(coins)
+        n = len(coins)
+        dp = [[float('inf')] * (amount + 1) for _ in range(n)]
+        dp = [float('inf') for _ in range(amount+1)]
+        
+        for a in range(amount + 1):
+            if a % coins[0] == 0:
+                dp[a] = a // coins[0]
 
-        dp = [[-1 for _ in range(amount+1)]for _ in range(n)]
-        ans = self.f(len(coins) - 1, amount, coins,dp)
+        
+        for i in range(1, n):
+            cur = [float('inf') for _ in range(amount+1)]
+            for a in range(amount + 1):
+                
+                notpick = dp[a]
+                pick = float('inf')
+                if a >= coins[i]:
+                    pick = 1 + cur[a - coins[i]]
+                cur[a] = min(pick, notpick)
+
+            dp =cur
+
+        ans = dp[amount]
         return -1 if ans == float('inf') else ans
