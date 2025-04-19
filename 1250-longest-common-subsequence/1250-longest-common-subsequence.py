fced237c1a410629efc380f7c63c84474fdb416d
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-04-19 22:07:16 +0530

    Time: 427 ms (81.02%), Space: 17.9 MB (93.5%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,25 +1,18 @@
 class Solution:
-    def f(self,i1,i2,t1,t2,dp):
-        if i1<0 or i2<0:
-            return 0
-
-        if dp[i1][i2]!=-1:
-            return dp[i1][i2]
+    def longestCommonSubsequence(self, t1: str, t2: str) -> int:
+        m, n = len(t1), len(t2)
         
-        if t1[i1]==t2[i2]: 
-            dp[i1][i2] = 1+self.f(i1-1,i2-1,t1,t2,dp)
-            return dp[i1][i2]
 
-        else:
-            dp[i1][i2] = max(self.f(i1-1,i2,t1,t2,dp),self.f(i1,i2-1,t1,t2,dp))
-            return dp[i1][i2]
+        dp = [0 for _ in range(n + 1)]
+        
+        for i in range(1, m + 1):
+            cur = [0 for _ in range(n + 1)]
+            for j in range(1, n + 1):
+                if t1[i - 1] == t2[j - 1]:
+                    cur[j] = 1 + dp[j - 1]
+                else:
+                    cur[j] = max(dp[j], cur[j - 1])
 
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        m=len(text1)
-        n = len(text2)
-
-        dp = [[-1 for _ in range(n)]for _ in range(m) ]
-
-
-
-        return self.f(m-1,n-1,text1,text2,dp)
+            dp = cur
+        
+        return dp[n]
