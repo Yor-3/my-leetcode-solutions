caf3f4d1ac1f3d722faebaac7ddc5ad1e9394177
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-04-19 23:29:35 +0530

    Time: 455 ms (49.4%), Space: 74.7 MB (38.24%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,22 +1,33 @@
 class Solution:
-    def f(self,i,j,s,t,dp):
-        if j<0:
-            return 1
-        if i<0:
-            return 0
+    # def f(self,i,j,s,t,dp):
+    #     if j<0:
+    #         return 1
+    #     if i<0:
+    #         return 0
 
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        if s[i]==t[j]:
-            dp[i][j] =  self.f(i-1,j-1,s,t,dp)+self.f(i-1,j,s,t,dp)
-            return dp[i][j]
-        else:
-            dp[i][j] = self.f(i-1,j,s,t,dp)
-        return dp[i][j]
+    #     if dp[i][j]!=-1:
+    #         return dp[i][j]
+    #     if s[i]==t[j]:
+    #         dp[i][j] =  self.f(i-1,j-1,s,t,dp)+self.f(i-1,j,s,t,dp)
+    #         return dp[i][j]
+    #     dp[i][j] = self.f(i-1,j,s,t,dp)
+    #     return dp[i][j]
     def numDistinct(self, s: str, t: str) -> int:
 
         m= len(s)
         n=len(t)
 
-        dp = [[-1 for _ in range(n)]for _ in range(m)]
-        return self.f(m-1,n-1,s,t,dp)
+        
+
+        dp = [[0 for _ in range(n+1)]for _ in range(m+1)]
+        for i in range(m + 1):
+            dp[i][0] = 1
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+
+        return dp[m][n]
+        
