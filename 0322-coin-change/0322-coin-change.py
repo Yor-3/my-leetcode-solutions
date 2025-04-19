0a9d387837faf0d65a709410f8ee5ea9464641a6
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-04-19 21:12:46 +0530

    Time: 0 ms (100%), Space: 18.3 MB (43.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,26 +1,40 @@
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
         
-        dp = [float('inf') for _ in range(amount+1)]
-        
-        for a in range(amount + 1):
-            if a % coins[0] == 0:
-                dp[a] = a // coins[0]
+        # def helper(amt, coins, dp):
+        #     if amt == 0:
+        #         return 0
+        #     elif amt < 0:
+        #         return float("inf")
 
-        
-        for i in range(1, n):
-            cur = [float('inf') for _ in range(amount+1)]
-            for a in range(amount + 1):
-                
-                notpick = dp[a]
-                pick = float('inf')
-                if a >= coins[i]:
-                    pick = 1 + cur[a - coins[i]]
-                cur[a] = min(pick, notpick)
+        #     if dp[amt] != -1:
+        #         return dp[amt]
 
-            dp =cur
+        #     mini = float("inf")
+        #     for i in range(len(coins)):
+        #         ans = helper(amt-coins[i], coins, dp)
+        #         if ans != float("inf"):
+        #             mini = min(mini, 1+ans)
+            
+        #     dp[amt] = mini
 
-        ans = dp[amount]
-        return -1 if ans == float('inf') else ans
+        #     return mini
+
+        # dp = [-1] * (amount+1)
+        # ans = helper(amount, coins, dp)
+        # return -1 if ans == float("inf") else ans
+   
+        # tabulation
+
+        dp = [float("inf")] * (amount+1)
+        dp[0] = 0
+
+        for amt in range(1, amount+1):
+            for coin in coins:
+                if amt-coin >= 0:
+                    dp[amt] = min(dp[amt], 1+ dp[amt-coin])
+
+        if dp[amount] != float('inf'):
+            return dp[amount]
+        return -1
