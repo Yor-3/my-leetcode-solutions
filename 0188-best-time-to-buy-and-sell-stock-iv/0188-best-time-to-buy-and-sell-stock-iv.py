6cf6932b689fee34cabd841d0ae8482a068cbfea
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-04-20 01:39:04 +0530

    Time: 67 ms (53.16%), Space: 17.9 MB (83.52%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self,k, prices: List[int]) -> int:
+        n = len(prices)
+        if n == 0:
+            return 0
+
+        after = [[0] * (k+1) for _ in range(2)]
+        curr = [[0] * (k+1) for _ in range(2)]
+
+        for i in range(n - 1, -1, -1):
+            for bs in range(2):
+                for cap in range(1, k+1):
+                    if bs:
+                        buy = -prices[i] + after[0][cap]
+                        skip = after[1][cap]
+                        curr[bs][cap] = max(buy, skip)
+                    else:
+                        sell = prices[i] + after[1][cap - 1]
+                        skip = after[0][cap]
+                        curr[bs][cap] = max(sell, skip)
+            after = [curr_row[:] for curr_row in curr]
+
+        return after[1][k]
