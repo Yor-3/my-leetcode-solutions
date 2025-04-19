0919dab88d4ebf5ecded296be4f4733e74c382ec
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-04-19 22:58:04 +0530

    Time: 199 ms (99.31%), Space: 55.8 MB (26.59%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.py
@@ -1,43 +1,63 @@
 class Solution:
-    def shortestCommonSupersequence(self, s1: str, s2: str) -> str:
-        n, m = len(s1), len(s2)
-        dp = [[0] * (m + 1) for _ in range(2)]
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        # Find starting common sequence.  If common start 
+        # covers all of one or both of the strings, then 
+        # return the common start string plus any remnants 
+        # of the other string.
+        cmnStart = ""
+        n = min(len(str1), len(str2))
+        i = 0
+        while i < n and str1[i] == str2[i]:  i += 1
+        if i == n:  
+            return "".join([str1[:i], str1[i:], str2[i:]])
 
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                if s1[i - 1] == s2[j - 1]:
-                    dp[i % 2][j] = 1 + dp[(i - 1) % 2][j - 1]
-                else:
-                    dp[i % 2][j] = max(dp[(i - 1) % 2][j], dp[i % 2][j - 1])
+        # Find ending common sequence.
+        cmnEnd = ""
+        n -= i
+        j = 1
+        while j < n and str1[-j] == str2[-j]:  j += 1
+        j -= 1
 
-        i, j = n, m
-        res = []
-        full_dp = [[0] * (m + 1) for _ in range(n + 1)]
+        # After finding common start and common end of strings, 
+        # remove the common start and end of the strings.
+        if i > 0 or j > 0:
+            if i > 0:  cmnStart = str1[:i]
+            if j > 0:  cmnEnd = str1[-j:]
+            str1 = str1[i: len(str1) - j]
+            str2 = str2[i: len(str2) - j]
+        n1 = len(str1)
+        n2 = len(str2)
 
-        for x in range(1, n + 1):
-            for y in range(1, m + 1):
-                if s1[x - 1] == s2[y - 1]:
-                    full_dp[x][y] = 1 + full_dp[x - 1][y - 1]
-                else:
-                    full_dp[x][y] = max(full_dp[x - 1][y], full_dp[x][y - 1])
+        # Find longest common sequence using DP.
+        sc1 = list(map(ord, str1))
+        sc2 = list(map(ord, str2))
+        dp = [[0]*(n2 + 1) for _ in range(n1 + 1)]
+        for i1 in range(n1 + 1):  dp[i1][0] = i1
+        dp[0] = list(range(n2 + 1))
 
-        while i > 0 and j > 0:
-            if s1[i - 1] == s2[j - 1]:
-                res.append(s1[i - 1])
-                i -= 1
-                j -= 1
-            elif full_dp[i - 1][j] > full_dp[i][j - 1]:
-                res.append(s1[i - 1])
-                i -= 1
+        for i1,ch1 in enumerate(sc1):
+            for i2,ch2 in enumerate(sc2):
+                dp[i1 + 1][i2 + 1] = 1 + (dp[i1][i2] if ch1 == ch2 else min(dp[i1 + 1][i2], dp[i1][i2 + 1]))
+        
+        # Build result string from backtracking through DP 
+        # for longest common sequence.
+        i1 = n1 - 1
+        i2 = n2 - 1
+        result = [cmnEnd]
+        while i1 >= 0 and i2 >= 0:
+            if sc1[i1] == sc2[i2]:
+                result.append(str1[i1])
+                i1 -= 1
+                i2 -= 1
+            elif dp[i1 + 1][i2 + 1] == dp[i1][i2 + 1] + 1:
+                result.append(str1[i1])
+                i1 -= 1
             else:
-                res.append(s2[j - 1])
-                j -= 1
+                result.append(str2[i2])
+                i2 -= 1
+        
+        if i2 >= 0:  result.append(str2[: i2 + 1])
+        if i1 >= 0:  result.append(str1[: i1 + 1])
+        result.append(cmnStart)
+        return "".join(reversed(result))
 
-        while i > 0:
-            res.append(s1[i - 1])
-            i -= 1
-        while j > 0:
-            res.append(s2[j - 1])
-            j -= 1
-
-        return ''.join(reversed(res))
