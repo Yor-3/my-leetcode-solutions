9e3483d246e0ac3eaf8dcd374de00fc6d802ce74
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-04-20 19:14:02 +0530

    Time: 3473 ms (5.38%), Space: 18.5 MB (9.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,9 +1,46 @@
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        LIS = [1] * len(nums)
+    # def f(self, i, p, nums, dp):
+    #     n = len(nums)
+    #     if i == n:
+    #         return 0
 
-        for i in range(len(nums) - 1, -1, -1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] < nums[j]:
-                    LIS[i] = max(LIS[i], 1 + LIS[j])
-        return max(LIS)
+    #     if dp[i][p + 1] != -1:
+    #         return dp[i][p + 1]
+
+    #     size = self.f(i + 1, p, nums, dp)
+    #     if p == -1 or nums[p] < nums[i]:
+    #         size = max(size, 1 + self.f(i + 1, i, nums, dp))
+
+    #     dp[i][p + 1] = size
+    #     return size
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        # dp = [[0 for _ in range(n + 1)] for _ in range(n+1)]
+        # for i in range(n-1,-1,-1):
+        #     for p in range(i-1,-2,-1):
+        #         size = dp[i+1][p+1]
+
+        #         if p ==-1 or nums[p]<nums[i]:
+
+        #             size = max(size,1+dp[i+1][i+1])
+
+        #         dp[i][p+1]=size
+
+        # return dp[0][0]
+        dp = [0 for _ in range(n+1)]
+        for i in range(n-1,-1,-1):
+            prev = [0 for _ in range(n+1)]
+            for p in range(i-1,-2,-1):
+                size = dp[p+1]
+                if p==-1 or nums[p]<nums[i]:
+                    size = max(size,1+dp[i+1])
+                prev[p+1] = size
+
+            dp = prev
+
+        return dp[0]
+
+
+
+        
