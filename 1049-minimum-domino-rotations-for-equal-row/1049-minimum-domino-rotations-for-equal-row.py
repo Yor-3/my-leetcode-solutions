30dc2d334d191ab88b7d3942c08639161b3c057a
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-05-03 08:09:00 +0530

    Time: 19 ms (98.52%), Space: 18.4 MB (92.19%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.py
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.py
@@ -1,41 +1,20 @@
-from collections import defaultdict
-
+from typing import List
 
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        up = defaultdict(int)
-        down = defaultdict(int)
+        def check(x):
+            rotate_top = rotate_bottom = 0
+            for t, b in zip(tops, bottoms):
+                if t != x and b != x:
+                    return float('inf')  
+                elif t != x:
+                    rotate_top += 1
+                elif b != x:
+                    rotate_bottom += 1
+            return min(rotate_top, rotate_bottom)
 
-        for t, b in zip(tops, bottoms):
-            up[t] += 1
-            down[b] += 1
+        candidate1 = tops[0]
+        candidate2 = bottoms[0]
 
-       
-        combined = defaultdict(int)
-        for key in set(up) | set(down):
-            combined[key] = up[key] + down[key]
-
-        needednum = -1
-        n = len(tops)
-
-        
-        for key in combined:
-            if combined[key] >= n:
-                needednum = key
-                break
-
-        if needednum == -1:
-            return -1
-
-        
-        rotate_top = 0
-        rotate_bottom = 0
-        for t, b in zip(tops, bottoms):
-            if t != needednum and b != needednum:
-                return -1  
-            if t != needednum:
-                rotate_top += 1
-            if b != needednum:
-                rotate_bottom += 1
-
-        return min(rotate_top, rotate_bottom)
+        res = min(check(candidate1), check(candidate2 if candidate1 != candidate2 else float('inf')))
+        return res if res != float('inf') else -1
