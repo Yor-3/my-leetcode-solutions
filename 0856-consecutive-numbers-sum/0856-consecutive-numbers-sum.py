4851fc94d1934ae6c574df2c65566ebeadc92523
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2025-05-03 11:31:19 +0530

    Time: 15 ms (95.26%), Space: 17.8 MB (41.5%) - LeetHub

--- 0856-consecutive-numbers-sum/0856-consecutive-numbers-sum.py
+++ 0856-consecutive-numbers-sum/0856-consecutive-numbers-sum.py
@@ -1,10 +1,20 @@
 class Solution:
     def consecutiveNumbersSum(self, n: int) -> int:
+        if n == 1:
+            return 1
         
-        count = 0
-        k = 1
-        while k * (k - 1) // 2 < n:
-            if (n - (k * (k - 1)) // 2) % k == 0:
-                count += 1
-            k += 1
-        return count
+        ans: int = 0
+        if n % 2:
+            ans = 2
+        else:
+            ans = 1
+        
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                if (n // i) % 2:
+                    ans += 1
+                if (i % 2) and i != n**0.5:
+                    ans += 1
+        
+        return ans
+        
