7a308f904060b20aadeb5ca48e6395260af3667e
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2026-01-17 10:22:42 +0530

    Time: 1 ms (14.39%), Space: 19.5 MB (6.11%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -4,8 +4,7 @@ class Solution:
 
         for i in range(n-1):
             temp = [0]+res[-1]+[0]
-            row=[]
-
+            row = []
             for j in range(len(res[-1])+1):
                 row.append(temp[j]+temp[j+1])
 
