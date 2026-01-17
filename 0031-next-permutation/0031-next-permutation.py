496fcf5c3260331dcaceebb3a1ba743be1f1d5a5
Yor-3 <88503542+Yor-3@users.noreply.github.com>
2026-01-17 10:57:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (12.36%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,14 +4,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        i = n-2
+        i = len(nums)-2
 
         while i>=0 and nums[i]>=nums[i+1]:
             i-=1
 
-        if i>=0:
-            j=n-1
-            while nums[j]<=nums[i]:
+        if i >=0:
+            j = n-1
+
+            while j>=0 and nums[i]>=nums[j]:
                 j-=1
 
             nums[i],nums[j] = nums[j],nums[i]
